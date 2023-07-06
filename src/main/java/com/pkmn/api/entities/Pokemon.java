24f20fbd5a47912077cb86ccfaf9b0821f92ad33
levimaciell <levimaciell1221@gmail.com>
2023-07-06 16:46:08 -0300

    Pokemon entity

---
+++ src/main/java/com/pkmn/api/entities/Pokemon.java
@@ -0,0 +1,193 @@
+package com.pkmn.api.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "pokemons")
+public class Pokemon {
+
+    @Id
+    @GeneratedValue
+    private int pokedexEntry;
+    private String name;
+    private String type1;
+    private String type2;
+    private int total;
+    private int hp;
+    private int attack;
+    private int defense;
+    private int spAttack;
+    private int spDefense;
+    private int speed;
+    private int generation;
+    private boolean legendary;
+
+    public Pokemon() {
+    }
+
+    //2 types pokemons
+    public Pokemon(int pokedexEntry, String name, String type1, String type2, int total, int hp, int attack,
+            int defense, int spAttack, int spDefense, int speed, int generation, boolean legendary) {
+        this.pokedexEntry = pokedexEntry;
+        this.name = name;
+        this.type1 = type1;
+        this.type2 = type2;
+        this.total = total;
+        this.hp = hp;
+        this.attack = attack;
+        this.defense = defense;
+        this.spAttack = spAttack;
+        this.spDefense = spDefense;
+        this.speed = speed;
+        this.generation = generation;
+        this.legendary = legendary;
+    }
+
+    //monotypes pokemons
+    public Pokemon(int pokedexEntry, String name, String type1, int total, int hp, int attack, int defense,
+            int spAttack, int spDefense, int speed, int generation, boolean legendary) {
+        this.pokedexEntry = pokedexEntry;
+        this.name = name;
+        this.type1 = type1;
+        this.total = total;
+        this.hp = hp;
+        this.attack = attack;
+        this.defense = defense;
+        this.spAttack = spAttack;
+        this.spDefense = spDefense;
+        this.speed = speed;
+        this.generation = generation;
+        this.legendary = legendary;
+    }
+
+    public int getPokedexEntry() {
+        return pokedexEntry;
+    }
+
+    public void setPokedexEntry(int pokedexEntry) {
+        this.pokedexEntry = pokedexEntry;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType1() {
+        return type1;
+    }
+
+    public void setType1(String type1) {
+        this.type1 = type1;
+    }
+
+    public String getType2() {
+        return type2;
+    }
+
+    public void setType2(String type2) {
+        this.type2 = type2;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public void setAttack(int attack) {
+        this.attack = attack;
+    }
+
+    public int getDefense() {
+        return defense;
+    }
+
+    public void setDefense(int defense) {
+        this.defense = defense;
+    }
+
+    public int getSpAttack() {
+        return spAttack;
+    }
+
+    public void setSpAttack(int spAttack) {
+        this.spAttack = spAttack;
+    }
+
+    public int getSpDefense() {
+        return spDefense;
+    }
+
+    public void setSpDefense(int spDefense) {
+        this.spDefense = spDefense;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public int getGeneration() {
+        return generation;
+    }
+
+    public void setGeneration(int generation) {
+        this.generation = generation;
+    }
+
+    public boolean islegendary() {
+        return legendary;
+    }
+
+    public void setlegendary(boolean legendary) {
+        this.legendary = legendary;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + pokedexEntry;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Pokemon other = (Pokemon) obj;
+        if (pokedexEntry != other.pokedexEntry)
+            return false;
+        return true;
+    }
+
+    
+}
