c2aa8ab21cb93e02101862ac99ec74e52a5fe7bd
levimaciell <levimaciell1221@gmail.com>
2023-08-16 14:06:48 -0300

    UserService and UserDto created

--- src/main/java/com/pkmn/api/user/repositories/UserRepository.java
+++ src/main/java/com/pkmn/api/user/repositories/UserRepository.java
@@ -1,9 +1,11 @@
 package com.pkmn.api.user.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.pkmn.api.user.entities.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User,Long>{
     
 }
---
+++ src/main/java/com/pkmn/api/user/services/UserService.java
@@ -0,0 +1,25 @@
+package com.pkmn.api.user.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.pkmn.api.user.dto.UserDto;
+import com.pkmn.api.user.repositories.UserRepository;
+
+@Service
+public class UserService {
+    
+    @Autowired
+    private UserRepository repository;
+
+    public List<UserDto> findAll(){
+        return repository.findAll().stream().map(x -> new UserDto(x)).toList();
+    }
+
+
+
+
+
+}
